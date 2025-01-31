db4c105c3dd3f2b421180aef31321fe55146aba4
panchaproduct <sadew4610@gmail.com>
2025-01-31 19:23:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "whZXiDDQ#_zklFVlZYKnbUGczkVz55ZEU5m2OD4Np6H65jemL_ug", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
-STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
+STATUES_REPLY: process.env.STATUES_REPLY || "false", //true or false
 AUTO_REACT: process.env.AUTO_REACT || "true", //true or false
 INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false
 READ_MESSAGE: process.env.READ_MESSAGE || "false", //true or false
